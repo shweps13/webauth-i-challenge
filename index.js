da49ba6e49c7e4060d0b71a15b9ab1f076ac0cb3
Heorhii Siburov <g.siburov@gmail.com>
2019-10-14 18:19:24 -0700

    model was added

--- index.js
+++ index.js
@@ -13,5 +13,23 @@ server.get('/', (req, res) => {
     res.send("That's working!");
 });
 
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+  
+    //hash the pass
+    const hash = bcrypt.hashSync(user.password, 8)
+  
+    //we override ht pass with the hash
+    user.password = hash;
+  
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
